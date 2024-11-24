40eda07b04f18a3271f03ef23460f69d39c3ffa4
frontcodelover <contact@avantjetaisriche.com>
2024-11-24 18:35:24 +0100

    fix: change name of TS props auth-button

--- application/front-end/src/presentation/login/components/auth-button.tsx
+++ application/front-end/src/presentation/login/components/auth-button.tsx
@@ -1,11 +1,11 @@
 import { Button } from '@/presentation/components/ui/button';
 
-type AuthCardProps = {
+type AuthButtonProps = {
   provider: 'Gmail' | 'Github';
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
 };
 
-export const AuthButton = ({ provider, icon: Icon }: AuthCardProps) => {
+export const AuthButton = ({ provider, icon: Icon }: AuthButtonProps) => {
   return (
     <Button className="flex w-full items-center gap-2">
       <Icon className="h-5 w-5" />
