f963ca401e844ac848424d7bd25526b05437322b
frontcodelover <contact@avantjetaisriche.com>
2024-11-24 18:41:08 +0100

    refact: extraction from page to auth-card

--- application/front-end/src/app/(auth)/login/page.tsx
+++ application/front-end/src/app/(auth)/login/page.tsx
@@ -4,13 +4,7 @@ import { AuthCard } from '@/presentation/login/components/auth-card';
 export default function LoginPage() {
   return (
     <div className="flex w-auto max-w-[400px] flex-col justify-center gap-8">
-      <Card className="flex flex-col gap-10 p-8">
-        <div>
-          <h1 className="text-2xl font-bold py-2">Se connecter</h1>
-          <p className="text-black/60">Envie de devenir mentor ou mentoré, connectez-vous pour commencer votre aventure !</p>
-        </div>
-        <AuthCard />
-      </Card>
+      <AuthCard />
     </div>
   );
 }
--- application/front-end/src/presentation/login/components/auth-card.tsx
+++ application/front-end/src/presentation/login/components/auth-card.tsx
@@ -1,12 +1,20 @@
+import { Card } from '@/presentation/components/ui/card';
 import { AuthButton } from './auth-button';
 import { Mail, Github } from 'lucide-react';
 
-
 export const AuthCard = () => {
   return (
-    <div className='flex flex-col w-80 items-center gap-4'>
-      <AuthButton provider={'Gmail'} icon={Mail} />
-      <AuthButton provider={'Github'} icon={Github} />
-    </div>
+    <Card className="flex flex-col gap-10 p-8">
+      <div>
+        <h1 className="py-2 text-2xl font-bold">Se connecter</h1>
+        <p className="text-black/60">
+          Envie de devenir mentor ou mentoré, connectez-vous pour commencer votre aventure !
+        </p>
+      </div>
+      <div className="flex w-80 flex-col items-center gap-4">
+        <AuthButton provider={'Gmail'} icon={Mail} />
+        <AuthButton provider={'Github'} icon={Github} />
+      </div>
+    </Card>
   );
 };
