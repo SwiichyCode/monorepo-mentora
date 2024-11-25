4ed8aa15dd7b8b440035df013258c462df9b03fe
SwiichyCode <adlpromail@gmail.com>
2024-11-25 03:33:01 +0100

    refact: implement back link

--- application/front-end/src/presentation/login/components/auth-card.tsx
+++ application/front-end/src/presentation/login/components/auth-card.tsx
@@ -1,5 +1,15 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/presentation/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/presentation/components/ui/card';
 import { OauthButton } from './oauth-button';
+import Link from 'next/link';
+import { Separator } from '@/presentation/components/ui/separator';
+import { PATH } from '@common/constants';
 
 type AuthCardProps = {
   title: string;
@@ -8,15 +18,21 @@ type AuthCardProps = {
 
 export const AuthCard = ({ title, description }: AuthCardProps) => {
   return (
-    <Card className="w-[380px] h-max m-auto space-y-8">
+    <Card className="w-[380px] h-max m-auto space-y-4">
       <CardHeader>
         <CardTitle className="text-2xl">{title}</CardTitle>
-        <CardDescription className="text-base">{description}</CardDescription>
+        <CardDescription>{description}</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col items-center gap-4">
         <OauthButton provider={'gmail'} />
+        <Separator />
         <OauthButton provider={'github'} />
       </CardContent>
+      <CardFooter>
+        <Link href={PATH.HOME} className="text-sm text-gray-500 hover:underline">
+          Retourner à l&apos;accueil
+        </Link>
+      </CardFooter>
     </Card>
   );
 };
--- package/common/src/constants/path.ts
+++ package/common/src/constants/path.ts
@@ -7,4 +7,5 @@ export const PATH = {
       MENTEE: '/auth/onboarding/mentee',
     },
   },
+  HOME: '/',
 };
