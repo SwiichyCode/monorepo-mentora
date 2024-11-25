0ab31c759672823235f39ee7db1073cd2faaaefa
SwiichyCode <adlpromail@gmail.com>
2024-11-25 02:59:11 +0100

    refact: component updates

--- application/front-end/src/app/auth/signin/page.tsx
+++ application/front-end/src/app/auth/signin/page.tsx
@@ -2,8 +2,9 @@ import { AuthCard } from '@/presentation/login/components/auth-card';
 
 export default function SignInPage() {
   return (
-    <div className="flex w-auto max-w-[400px] flex-col justify-center">
-      <AuthCard />
-    </div>
+    <AuthCard
+      title="Se connecter"
+      description="Envie de devenir mentor ou mentoré, connectez-vous pour commencer votre aventure !"
+    />
   );
 }
--- application/front-end/src/presentation/login/components/auth-card.tsx
+++ application/front-end/src/presentation/login/components/auth-card.tsx
@@ -1,20 +1,23 @@
-import { Card } from '@/presentation/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/presentation/components/ui/card';
 import { AuthButton } from './auth-button';
 import { Mail, Github } from 'lucide-react';
 
-export const AuthCard = () => {
+type AuthCardProps = {
+  title: string;
+  description: string;
+};
+
+export const AuthCard = ({ title, description }: AuthCardProps) => {
   return (
-    <Card className="flex flex-col gap-10 p-8">
-      <div>
-        <h1 className="py-2 text-2xl font-bold">Se connecter</h1>
-        <p className="text-black/60">
-          Envie de devenir mentor ou mentoré, connectez-vous pour commencer votre aventure !
-        </p>
-      </div>
-      <div className="flex w-80 flex-col items-center gap-4">
+    <Card className="w-[380px] h-max m-auto space-y-8">
+      <CardHeader>
+        <CardTitle className="text-2xl">{title}</CardTitle>
+        <CardDescription className="text-base">{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center gap-4">
         <AuthButton provider={'Gmail'} icon={Mail} />
         <AuthButton provider={'Github'} icon={Github} />
-      </div>
+      </CardContent>
     </Card>
   );
 };
