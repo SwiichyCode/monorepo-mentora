f912fa9085f9b6cd85b61b48f172993b04313f79
SwiichyCode <adlpromail@gmail.com>
2024-11-25 02:37:11 +0100

    refact: uptate user model

--- package/domain/src/entities/user.entity.ts
+++ package/domain/src/entities/user.entity.ts
@@ -4,8 +4,16 @@ export class User {
   readonly email: string | null;
   readonly emailVerified: Date | null;
   readonly image: string | null;
+  readonly picture: string | null;
+  readonly pseudo: string;
+  readonly bio: string | null;
+  readonly stacks: string[] | null;
+  readonly expertise: Expertise[] | null;
+  readonly linkedin: string | null;
+  readonly twitter: string | null;
+  readonly github: string | null;
+  readonly website: string | null;
   readonly role: Role;
-  readonly points: number;
 
   constructor(props: User) {
     this.id = props.id;
@@ -13,9 +21,19 @@ export class User {
     this.email = props.email;
     this.emailVerified = props.emailVerified;
     this.image = props.image;
+    this.picture = props.picture;
+    this.pseudo = props.pseudo;
+    this.bio = props.bio;
+    this.stacks = props.stacks;
+    this.expertise = props.expertise;
+    this.linkedin = props.linkedin;
+    this.twitter = props.twitter;
+    this.github = props.github;
+    this.website = props.website;
     this.role = props.role;
-    this.points = props.points;
   }
 }
 
-export type Role = 'USER' | 'COLLABORATOR' | 'ADMIN';
+export type Role = 'MENTOR' | 'MENTEE' | 'ADMIN';
+
+export type Expertise = 'FRONTEND' | 'BACKEND' | 'FULLSTACK';
